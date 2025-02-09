98593cafa40ae85343e1938a52f64affd15801b8
Drfinik <freelanceap@yahoo.com>
2025-02-09 14:47:29 +0300

    1_module solution to task-1,2,3,4,5

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,8 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  return name !== null && name !== undefined && !name.includes(" ") && name.length >= 4;
+
 }
 
 function sayHello() {
