0caf916a77a00e4fd0e5acdfcae6f3ffd46dabf4
Drfinik <freelanceap@yahoo.com>
2025-02-16 19:32:04 +0300

    3_module solution to task-1,2,3,4,5

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,23 @@
 function showSalary(users, age) {
-  // ваш код...
+  let result = '';
+
+  for (let i = 0; i < users.length; i++) {
+    const user = users[i];
+    if (user.age <= age) {
+      result += `${user.name}, ${user.balance}`;
+      if (i < users.length - 1) {
+        let nextUserIsEligible = false;
+        for (let j = i + 1; j < users.length; j++) {
+          if (users[j].age <= age) {
+            nextUserIsEligible = true;
+            break;
+          }
+        }
+        if (nextUserIsEligible)
+          result += '\n';
+      }
+    }
+  }
+
+  return result;
 }
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,15 @@
 function getMinMax(str) {
-  // ваш код...
+  const numbers = str
+    .split(' ')
+    .map(Number)
+    .filter(Number.isFinite);
+
+  if (numbers.length === 0) {
+    return { min: NaN, max: NaN };
+  }
+
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+
+  return { min, max };
 }
