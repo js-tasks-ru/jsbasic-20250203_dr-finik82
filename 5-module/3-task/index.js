fcae114c1f111f19cb9b9359bc6d0e5397fba866
Drfinik <freelanceap@yahoo.com>
2025-02-20 19:49:36 +0300

    Refactor: Made edits to task 3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,36 +1,36 @@
 function initCarousel() {
+  const carouselInner = document.querySelector('.carousel__inner');
   const carouselArrowRight = document.querySelector('.carousel__arrow_right');
   const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
-  const slides = document.querySelectorAll('.carousel__slide');
-  let slideIndex = 1;
+  const carouselSlideWidth = document.querySelector('.carousel__slide').offsetWidth;
+  const totalSlides = document.querySelectorAll('.carousel__slide').length;
+  let currentSlide = 0;
 
+  carouselArrowLeft.style.display = 'none';
 
-  carouselArrowLeft.style.display = 'none'; 
-  
+  function updateCarousel() {
+    carouselInner.style.transform = `translateX(-${currentSlide * carouselSlideWidth}px)`;
 
-  carouselArrowRight.addEventListener('click', () =>{
-    if (slideIndex < slides.length) {
-      slides[slideIndex - 1].style.display = 'none';
-      slideIndex++;
-      slides[slideIndex - 1].style.display = 'block';
-      if (slideIndex === slides.length) {
-        carouselArrowRight.style.display = 'none';
-      }else {
-        carouselArrowLeft.style.display = 'block';
-      }
+    if (currentSlide === 0) {
+      carouselArrowLeft.style.display = 'none';
+    } else {
+      carouselArrowLeft.style.display = '';
     }
+
+    if (currentSlide === totalSlides - 1) {
+      carouselArrowRight.style.display = 'none';
+    } else {
+      carouselArrowRight.style.display = '';
+    }
+  }
+
+  carouselArrowRight.addEventListener('click', () => {
+    currentSlide++;
+    updateCarousel();
   });
 
   carouselArrowLeft.addEventListener('click', () => {
-    if (slideIndex > 1) {
-      slides[slideIndex - 1].style.display = 'none';
-      slideIndex--;
-      slides[slideIndex - 1].style.display = 'block';
-      if (slideIndex === 1) {
-        carouselArrowLeft.style.display = 'none';
-      }else{
-        carouselArrowRight.style.display = 'block';
-      }
-    }
+    currentSlide--;
+    updateCarousel();
   });
 }
