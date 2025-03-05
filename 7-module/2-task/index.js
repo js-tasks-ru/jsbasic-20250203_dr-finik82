94f2a15a789a62d0f32892fe358088dc15fb3cc7
Drfinik <freelanceap@yahoo.com>
2025-03-05 21:12:56 +0300

    7_module solution to task-1,2,3,4

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,102 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.modal = null;
+    this.closeButton = null;
+    this.body = document.body;
+    this.keydownHandler = this.keydownHandler.bind(this);
+    this.closeByClick = this.closeByClick.bind(this);
+  }
+
+  open() {
+    this.render();
+    this.body.classList.add('is-modal-open');
+    this.addEventListeners();
+  }
+
+  close() {
+    if (!this.modal) {
+      return;
+    }
+
+    this.removeEventListeners();
+    this.modal.remove();
+    this.body.classList.remove('is-modal-open');
+    this.modal = null;
+  }
+
+  setTitle(title) {
+    this.title = title;
+    if (this.modal) {
+      const titleElement = this.modal.querySelector('.modal__title');
+      if (titleElement) {
+        titleElement.textContent = title;
+      }
+    }
+  }
+
+  setBody(node) {
+    this.bodyContent = node;
+    if (this.modal) {
+      const bodyElement = this.modal.querySelector('.modal__body');
+      if (bodyElement) {
+        bodyElement.innerHTML = '';
+        bodyElement.append(node); 
+      }
+    }
+  }
+
+  render() {
+    this.modal = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 class="modal__title"></h3>
+          </div>
+          <div class="modal__body"></div>
+        </div>
+      </div>
+    `);
+
+    if (this.title) {
+      const titleElement = this.modal.querySelector('.modal__title');
+      if (titleElement) {
+        titleElement.textContent = this.title;
+      }
+    }
+    if (this.bodyContent) {
+      const bodyElement = this.modal.querySelector('.modal__body');
+      if (bodyElement) {
+        bodyElement.innerHTML = '';
+        bodyElement.append(this.bodyContent);
+      }
+    }
+
+    this.body.append(this.modal);
+    this.closeButton = this.modal.querySelector('.modal__close');
+  }
+
+  addEventListeners() {
+    this.closeButton.addEventListener('click', this.closeByClick);
+    document.addEventListener('keydown', this.keydownHandler);
+  }
+
+  removeEventListeners() {
+    this.closeButton.removeEventListener('click', this.closeByClick);
+    document.removeEventListener('keydown', this.keydownHandler);
+  }
+
+  keydownHandler(event) {
+    if (event.code === 'Escape') {
+      this.close();
+    }
+  }
+
+  closeByClick() {
+    this.close();
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,76 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.steps = steps;
+    this.value = value;
+
+    this.render();
+
+    this.addEventListeners();
+  }
+
+  render() {
+    this.elem = document.createElement('div');
+    this.elem.classList.add('slider');
+
+    this.elem.innerHTML = `
+      <div class="slider__thumb">
+        <span class="slider__value">${this.value}</span>
+      </div>
+      <div class="slider__progress"></div>
+      <div class="slider__steps">
+        ${'<span></span>'.repeat(this.steps)}
+      </div>
+    `;
+
+    this.thumb = this.elem.querySelector('.slider__thumb');
+    this.progress = this.elem.querySelector('.slider__progress');
+    this.valueElem = this.elem.querySelector('.slider__value');
+    this.stepsContainer = this.elem.querySelector('.slider__steps');
+    this.updateActiveStep();
+    this.updatePosition();
+  }
+
+  updateActiveStep() {
+    const stepElements = this.stepsContainer.querySelectorAll('span');
+
+    stepElements.forEach((step, index) => {
+      if (index === this.value) {
+        step.classList.add('slider__step-active');
+      } else {
+        step.classList.remove('slider__step-active');
+      }
+    });
+  }
+
+  updatePosition() {
+    let valuePercents = (this.value / (this.steps - 1)) * 100;
+
+    this.thumb.style.left = `${valuePercents}%`;
+    this.progress.style.width = `${valuePercents}%`;
+
+    this.valueElem.textContent = this.value;
+  }
+
+  addEventListeners() {
+    this.elem.addEventListener('click', this.onClick);
+  }
+
+  onClick = (event) => {
+    let left = event.clientX - this.elem.getBoundingClientRect().left;
+    let leftRelative = left / this.elem.offsetWidth;
+    let segments = this.steps - 1;
+    let approximateValue = leftRelative * segments;
+
+    this.value = Math.round(approximateValue);
+
+    this.updateActiveStep();
+    this.updatePosition();
+
+    this.elem.dispatchEvent(
+      new CustomEvent('slider-change', {
+        detail: this.value,
+        bubbles: true
+      })
+    );
   }
 }
