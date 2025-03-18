da4325346af1f0a547d84ca2becc4cd8e5424f06
Drfinik <freelanceap@yahoo.com>
2025-03-18 18:09:42 +0300

    8_module solution to task-1,2,3,4

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -26,7 +26,7 @@ export default class CartIcon {
       this.elem.classList.add('shake');
       this.elem.addEventListener('transitionend', () => {
         this.elem.classList.remove('shake');
-      }, {once: true});
+      }, { once: true });
 
     } else {
       this.elem.classList.remove('cart-icon_visible');
@@ -39,6 +39,44 @@ export default class CartIcon {
   }
 
   updatePosition() {
-    // ваш код ...
+    if (!this.elem.offsetWidth) return;
+
+    if (!this.initialTopCoord) {
+      this.initialTopCoord = this.elem.getBoundingClientRect().top + window.pageYOffset;
+    }
+
+    let isMobile = document.documentElement.clientWidth <= 767;
+
+    if (isMobile) {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+      return;
+    }
+
+    if (window.pageYOffset > this.initialTopCoord) {
+      let leftIndent = Math.min(
+        document.querySelector('.container').getBoundingClientRect().right + 20,
+        document.documentElement.clientWidth - this.elem.offsetWidth - 10
+      ) + 'px';
+
+      Object.assign(this.elem.style, {
+        position: 'fixed',
+        top: '50px',
+        zIndex: 1e3,
+        right: '10px',
+        left: leftIndent
+      });
+    } else {
+      Object.assign(this.elem.style, {
+        position: '',
+        top: '',
+        left: '',
+        zIndex: ''
+      });
+    }
   }
 }
--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,41 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+    this.render();
+  }
+
+  render() {
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner"></div>
+      </div>
+    `);
+
+    this.renderProducts();
+  }
+
+  renderProducts() {
+    const gridInner = this.elem.querySelector('.products-grid__inner');
+    gridInner.innerHTML = '';
+
+    for (let product of this.products) {
+      if (this.isProductVisible(product)) {
+        const productCard = new ProductCard(product);
+        gridInner.append(productCard.elem);
+      }
+    }
+  }
+
+  isProductVisible(product) {
+    if (this.filters.noNuts && product.nuts) return false;
+    if (this.filters.vegeterianOnly && !product.vegeterian) return false;
+    if (this.filters.maxSpiciness !== undefined && product.spiciness > this.filters.maxSpiciness) return false;
+    if (this.filters.category && product.category !== this.filters.category) return false;
+    return true;
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+    this.renderProducts();
   }
 }
